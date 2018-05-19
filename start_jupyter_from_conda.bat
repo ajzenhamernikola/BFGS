5d85d79e5bb2b4896f11fef770ac1e5621df6628
theikeofficial <ajzenhamernikola@gmail.com>
2018-05-19 17:19:33 +0200

    Fixed the starting script

--- start_jupyter_from_conda.bat
+++ start_jupyter_from_conda.bat
@@ -1,12 +1,15 @@
 @echo off
 
 :: Main
+setlocal enabledelayedexpansion
 if not exist "%ANACONDA3_ROOT%" ( 
-	set /p answer=Environment variable ANACONDA3_ROOT is not set. Would you like to use the default location: D:\Users\User\Anaconda3? [y/n] 
-	if "%answer%"=="y" ( 
+	@echo Environment variable ANACONDA3_ROOT is not set. Would you like to use the default location: D:\Users\User\Anaconda3? [y/n]
+	set /P answer=
+	if "!answer!"=="y" ( 
 		set ANACONDA3_ROOT=D:\Users\User\Anaconda3
 	) else (
-		set /p a3_path=Please enter the instalation directory for Anaconda3: 
+		@echo Please enter the instalation directory for Anaconda3:
+		set /P a3_path=
 		set ANACONDA3_ROOT="%a3_path%"
 	)
 )
@@ -16,7 +19,9 @@ exit /b
 
 :start_jupyter
 pushd .
-call %ANACONDA3_ROOT%\Scripts\activate.bat %ANACONDA3_ROOT%
+@echo Starting Anaconda3 environment...
+call !ANACONDA3_ROOT!\Scripts\activate.bat !ANACONDA3_ROOT!
 popd
+@echo Starting Jupyter notebook in this directory...
 jupyter notebook
 exit /b
